034666cc8da178814e2e5ec11be3f5a3507368a4
OlesiaChirkova <chirkova.o@gmail.com>
2025-08-06 08:51:32 +0200

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ Бот работает!")
 
-app = ApplicationBuilder().token("8220413192:AAGupQhhdCxuZmrEwTHkOHjg2v9QcEkxDrU").build()
+app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 
 app.add_handler(CommandHandler("start", start))
 
